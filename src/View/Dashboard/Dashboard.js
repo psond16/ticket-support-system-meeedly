791a0ec3ebace5b1e63d8eb8d51a1fe3a88f011e
Parineet <psond@uic.edu>
2026-04-03 15:04:35 -0500

    dashboard to ticket creation page

--- src/View/Dashboard/Dashboard.js
+++ src/View/Dashboard/Dashboard.js
@@ -1,8 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import TicketCard from "../../Components/TicketCard/TicketCard";
 
 function Dashboard({ tickets }) {
+    const navigate = useNavigate();
+
     return (
+        
         <div>
+            <button onClick={() => navigate("/create-ticket")}>
+                Create New Ticket
+            </button>
+            
             <h1>Dashboard</h1>
         </div>
     );
