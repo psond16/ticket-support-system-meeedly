7c99708dce3e11db8b242bfaff033b286e7ebf79
Parineet <psond@uic.edu>
2026-04-03 19:13:16 -0500

    figured out the ticket card UI using NOPLIN UI

--- src/Components/TicketCard/TicketCard.js
+++ src/Components/TicketCard/TicketCard.js
@@ -1,19 +1,54 @@
-function TicketCard({ ticket, deleteTicket }) {
-    return (
-        <div>
-            <h3>{ticket.title}</h3>
-            <p>{ticket.description}</p>
-            <button onClick={() => deleteTicket(ticket.id)}>Delete</button>
-            <p><strong>Category:</strong> {ticket.category}</p>
-            <p>Status: {ticket.status}</p>
-            <p><strong>Priority:</strong> {ticket.priority}</p>
-            <p><strong>Created:</strong> {ticket.createdAt}</p>
-            <p><strong>Assigned To:</strong> {ticket.assignedTo || "Unassigned"}</p>
-            <p><strong>User:</strong> {ticket.firstName} {ticket.lastName}</p>
-            <p><strong>Email:</strong> {ticket.email}</p>
+import {
+    NoplinCardGeneral,
+    NoplinCardBodyArea
+} from "noplin-uis";
 
-            {ticket.attachment && <p>Attachment included</p>}
-        </div>
+import "../../Style/Components/TicketCard/TicketCard.css";
+
+function TicketCard({ ticket }) {
+    return (
+        <NoplinCardGeneral className="ticket-card">
+
+            <NoplinCardBodyArea>
+                <div style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    width: "100%"
+                }}>
+
+                    {/* LEFT */}
+                    <div className="ticket-left">
+                       
+
+                        <div>
+                            <h4>{ticket.title}</h4>
+                            <p>{ticket.description}</p>
+                        </div>
+                    </div>
+
+                    {/* MIDDLE */}
+                    <div className="ticket-middle">
+                        <p>{ticket.category}</p>
+                        <p>{ticket.priority}</p>
+                        <p>{ticket.status}</p>
+                    </div>
+
+                    {/* RIGHT */}
+                    <div className="ticket-actions">
+                        <button onClick={() => console.log("delete", ticket.id)}>
+                            Delete
+                        </button>
+
+                        <button onClick={() => console.log("toggle status", ticket.id)}>
+                            Toggle
+                        </button>
+                    </div>
+
+                </div>
+            </NoplinCardBodyArea>
+
+        </NoplinCardGeneral>
     );
 }
 
--- src/View/Dashboard/Dashboard.js
+++ src/View/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import TicketCard from "../../Components/TicketCard/TicketCard";
+import "../../Style//Dashboard/Dashboard.css";
 
 function Dashboard({ tickets, deleteTicket }) {
     const navigate = useNavigate();
@@ -14,16 +15,18 @@ function Dashboard({ tickets, deleteTicket }) {
             <button onClick={() => navigate("/create-ticket")}>Create New Ticket</button>
 
             {/* show ticket / empty state */}
-            {tickets.length === 0 ? (
-                <p>No tickets yet</p>
-            ) : (
-                tickets.map((ticket) => (
-                    <TicketCard key={ticket.id} 
-                    ticket={ticket}
-                    deleteTicket={deleteTicket}
-                    />
-                ))
-            )}
+            <div className="dashboard-container">
+                {tickets.length === 0 ? (
+                    <p>No tickets yet</p>
+                ) : (
+                    tickets.map((ticket) => (
+                        <TicketCard key={ticket.id} 
+                        ticket={ticket}
+                        deleteTicket={deleteTicket}
+                        />
+                    ))
+                )}
+            </div>
         </div>
     );
 }
