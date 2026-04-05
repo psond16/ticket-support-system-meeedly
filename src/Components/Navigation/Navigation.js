5625f81c6134ac0314153c08504a8205fbeb1386
Parineet <psond@uic.edu>
2026-04-05 17:29:53 -0500

    updated navbar layout

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,18 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import logo from "../../Asset/Logo/meeedlyLogo.png";
 import "../../Style/Components/Navigation/Navigation.css";
 
 const Navigation = () => {
+
+    const navigate = useNavigate();
     return (
         <nav className="navbar">
-        <Link to= "/" className="navbar-brand">
-            < img src = {logo} alt="Meeedly Logo" className="navbar-logo" />
-        </Link>
-        <div className="navbar-links">
-            <Link to="/">Home</Link>
-            <Link to="/dashboard">Dashboard</Link>
-            <Link to="/about">About</Link>
-            <Link to="/settings">Settings</Link>
+            {/* LEFT SIDE */}
+            <div className="navbar-left">
+                <Link to="/" className="navbar-brand">
+                    <img src={logo} alt="Meeedly Logo" className="navbar-logo" />
+                </Link>
+
+                <div className="navbar-links">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/dashboard">Dashboard</NavLink>
+                    <NavLink to="/about">About</NavLink>
+                    <NavLink to="/settings">Settings</NavLink>
+                </div>
+            </div>
+
+            {/* RIGHT SIDE */}
+            <div className="navbar-right">
+                <button 
+                    className="create-ticket-btn"
+                    onClick={() => navigate("/create-ticket")}
+                >
+                    + Create Ticket
+                </button>
             </div>
         </nav>
     );
